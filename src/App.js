3fc4d4ebf7d6e77278874c4cf9dd7350bb33a8f8
ryutaro-kimura <sasuker2264@gmail.com>
2022-09-20 06:53:39 +0900

    first dnd app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1 style={{ marginBottom: "20px" }}>Trello風アプリ</h1>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
